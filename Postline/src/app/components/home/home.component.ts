e8401cbdbf48d2b40d6b0031932e6d06907adcd1
Task-Portal <82015387+Task-Portal@users.noreply.github.com>
2022-06-29 18:03:56 +0300

    getting all posts changed

--- Postline/src/app/components/home/home.component.ts
+++ Postline/src/app/components/home/home.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { PostsRepositoryService } from '../../services/repositories/posts-repository.service';
 import { IPost } from '../../interfaces/ipost';
 import { first } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ErrorHandlerService } from '../../services/error-handler.service';
+import { AlertService } from '../../services/alert.service';
+import { posts } from '../../routes/posts';
 
 @Component({
   selector: 'app-home',
@@ -9,14 +13,34 @@ import { first } from 'rxjs';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private getPostService: PostsRepositoryService) {}
+  //#region for alert
+  options = {
+    autoClose: true,
+    keepAfterRouteChange: true,
+  };
+  //endregion
+
+  constructor(
+    private repository: PostsRepositoryService,
+    private errorHandler: ErrorHandlerService,
+    private alert: AlertService
+  ) {}
 
   posts: IPost[];
 
   ngOnInit(): void {
-    this.getPostService
-      .getAllPosts()
-      .pipe(first())
-      .subscribe((posts) => (this.posts = posts));
+    this.getAllPosts();
   }
+
+  private getAllPosts = () => {
+    this.repository.getPosts(posts.getAllPost).subscribe({
+      next: (posts: IPost[]) => {
+        this.posts = posts;
+      },
+      error: (err: HttpErrorResponse) => {
+        this.errorHandler.handleError(err);
+        this.alert.error(this.errorHandler.errorMessage, this.options);
+      },
+    });
+  };
 }
--- Postline/src/app/services/repositories/posts-repository.service.ts
+++ Postline/src/app/services/repositories/posts-repository.service.ts
@@ -1,21 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IPost } from '../../interfaces/ipost';
 import { environment } from '../../../environments/environment';
+import { EnvironmentUrlService } from '../environment-url.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostsRepositoryService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private envUrl: EnvironmentUrlService
+  ) {}
 
-  public getAllPosts(): Observable<IPost[]> {
-    return this.httpClient.get<IPost[]>(`${environment.baseUrl}/post`);
-  }
-  public getUserPosts(id: string): Observable<IPost[]> {
-    return this.httpClient.get<IPost[]>(
-      `${environment.baseUrl}/post/user/${id}`
+  public getPosts = (route: string) => {
+    return this.http.get<IPost[]>(
+      this.createCompleteRoute(route, this.envUrl.urlAddress)
     );
+  };
+
+  public getUserPosts(id: string): Observable<IPost[]> {
+    return this.http.get<IPost[]>(`${environment.baseUrl}/post/user/${id}`);
   }
+
+  private createCompleteRoute = (route: string, envAddress: string) => {
+    return `${envAddress}/${route}`;
+  };
+
+  private generateHeaders = () => {
+    return {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+    };
+  };
 }
